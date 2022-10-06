481437f37c004937171eb59ae4b4e3691c49282f
flow_biboux <florianesm@gmail.com>
2022-10-07 00:47:33 +0200

    phantom

--- create-react-app-starter-copy/src/App.tsx
+++ create-react-app-starter-copy/src/App.tsx
@@ -1,7 +1,7 @@
-import { Adapter, WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { clusterApiUrl } from '@solana/web3.js';
 import { FC, ReactNode, useMemo } from 'react';
 import { Solana } from './solana';
@@ -25,10 +25,7 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
     // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-    const wallets = useMemo(() => [
-        new PhantomWalletAdapter() , 
-        // new SolflareWalletAdapter() as Adapter
-    ], []);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
     return (
         <ConnectionProvider endpoint={endpoint}>
